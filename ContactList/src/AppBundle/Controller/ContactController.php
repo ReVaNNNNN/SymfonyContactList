fa33b5d2ead2642c82ee5baf9b64eaccdb69f737
Kamil Sobecki <revannn5480@gmail.com>
2017-02-19 23:15:47 +0100

    Dodanie akcji wyswietlania, modyfikowania i usuwania pojedynczego kontaktu + widoki do tych akcji

--- ContactList/src/AppBundle/Controller/ContactController.php
+++ ContactList/src/AppBundle/Controller/ContactController.php
@@ -12,36 +12,40 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends Controller
 {
+    //formularz dodania nowej osoby do kontaktów
     /**
      * @Route("/new")
      * @Template()
      * @Method("GET")
      */
     public function newAction()
-    {
+    {   // tworzymy nowy obiekt na podstawie encji(klasy) Contact
         $contact = new Contact();
-
+        // tworzymy formularz dzięki metodzie z klasy Controller, potrzebujemy do tego stworzyć obiekt klasy ContactType
+        // w którym określone są pola naszego formularza oraz wcześniej już utworzony obiekt $contact
         $form = $this->createForm(new ContactType(), $contact,
-                                    [                               // tutaj zmienic na new ?
-                                    'action' => $this->generateUrl('app_contact_create')
+                                    [
+                                    // w tym miejscu dodajemy przekierowanie po wysłaniu formularza metoda POST
+                                    // (czyli kliknięciu w submit) do poniższego kontrolera
+                                    // który to nasze dane z formularza zapisze w bazie danych
+                                    'action' => $this->generateUrl('app_contact_new')
                                     ]);
-        // nie działa submit w url nowy po wpisaniu danych do formularza i probie wyslania ich
-        // w ogole nie reaguje
-        // dodac metode POST i URL new ponizej
-        // natomiast poprawnie dodaje do bazy
+        // zwracamy do widoku metodę która zbuduje nasz formularz
         return ['form' => $form->createView()];
     }
 
+    // dodanie danych z formularza do bazy danych jako nowy rekord w tabeli Contact
     /**
-     * @Route("/create")
+     * @Route("/new")
      * @Template()
+     * @Method("POST")
      */
     public function createAction(Request $request)
-    {
+    {   // tworzymy nowy obiekt na podstawie encji(klasy) Contact
         $contact = new Contact();
-
+        // tworzymy formularz dzięki metodzie z klasy Controller
         $form = $this->createForm(new ContactType(), $contact);
-
+        // UZUPEŁNIENIE KOMENTARZY
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()) {
@@ -49,15 +53,16 @@ class ContactController extends Controller
             $em->persist($contact);
             $em->flush();
 
-            return $this->redirectToRoute('app_contact_show',
-                                        [
-                                            'id' => $contact->getId()
-                                        ]);
+            return $this->redirectToRoute('app_contact_showall'
+                                       );
         }
 
         return ['form' => $form->createView()];
     }
 
+    // DODAĆ WYSWIETELNIE 1 CONTACTU
+
+    // wyświetlenie wszystkich obiektów z tabeli Contact
     /**
      * @Route("/show")
      * @Template()
@@ -71,5 +76,58 @@ class ContactController extends Controller
         }
 
         return ['contact' => $contact];
+    }
+    // edycja pojedyńczego obiektu z tabeli Contact
+    /**
+     * @Route("/{id}/modify")
+     * @Template()
+     */
+    public function modifyAction(Request $request, $id)
+    {
+        $contact = $this->getDoctrine()->getRepository('AppBundle:Contact')->find($id);
+
+        if(!$contact) {
+            throw $this->createNotFoundException('There is no contact with '. $id . ' ID.');
+        }
+
+        $form = $this->createForm(new ContactType(), $contact);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            return $this->redirectToRoute('app_contact_showall');
+        }
+
+        return ['form' => $form->createView()];
+    }
+
+    /**
+     * @Route("/{id}/delete")
+     * @Template()
+     */     // usuwanie działą poprawnie natomiast zastanowić się nad formą tego usuwania, nie powinno to być
+            // wyświetlone w formularzach, albo jako zwykly tekst albo od razu usuniecie + wyswietlenie komunikata
+            // + moze przekierowanie ? albo widok z akcja na metodzie POST(wykona sie po kliknieciu DELETE)
+    public function deleteAction(Request $request, $id)
+    {
+        $contact = $this->getDoctrine()->getRepository('AppBundle:Contact')->find($id);
+
+        if(!$contact) {
+            throw $this->createNotFoundException('There is no contact with '. $id . ' ID.');
+        }
+
+        $form = $this->createForm(new ContactType(), $contact);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($contact);
+            $em->flush();
+
+            return $this->redirectToRoute('app_contact_showall');
+        }
+
+        return ['form' => $form->createView()];
     }
 }
